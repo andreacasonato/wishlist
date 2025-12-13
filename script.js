d31ffd4f7dbcfe2ae46ded654fd1c54349f6c17c
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-12-13 19:19:34 +0100

    Add ability to enter text

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const input = document.getElementById("input");
 const button = document.getElementById("button");
+const wishesDisplay = document.getElementById("wishes-display");
+
+button.addEventListener("click", () => {
+  const newWishText = input.value; // save text user typed
+
+  if (newWishText.trim() === "") {
+    alert("Please enter a wish!");
+    return; // stop function if input empty
+  }
+
+  wishesDisplay.innerHTML = ""; // clear placeholder item
+
+  const wishItem = document.createElement("div");
+  wishItem.textContent = newWishText;
+
+  wishesDisplay.appendChild(wishItem); // attach new wish to the display container
+
+  input.value = ""; // clear input field
+});
